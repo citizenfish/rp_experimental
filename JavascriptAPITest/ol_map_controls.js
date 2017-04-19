3614d0062fbbce9b10f521bee2a123ef9f7bd2a8
Dave Barter <dave@phased.co.uk>
2017-04-19 22:58:32 +0100

    Fix GP

--- JavascriptAPITest/ol_map_controls.js
+++ JavascriptAPITest/ol_map_controls.js
@@ -1,4 +1,70 @@
 var map;
+var defaultStyle = {
+    'Point': new ol.style.Style({
+      image: new ol.style.Circle({
+        fill: new ol.style.Fill({
+          color: 'rgba(255,255,0,0.5)'
+        }),
+        radius: 5,
+        stroke: new ol.style.Stroke({
+          color: '#ff0',
+          width: 1
+        })
+      })
+    }),
+    'LineString': new ol.style.Style({
+      stroke: new ol.style.Stroke({
+        color: '#f00',
+        width: 3
+      })
+    }),
+    'Polygon': new ol.style.Style({
+      fill: new ol.style.Fill({
+        color: 'rgba(0,255,255,0.5)'
+      }),
+      stroke: new ol.style.Stroke({
+        color: '#0ff',
+        width: 1
+      })
+    }),
+    'MultiPoint': new ol.style.Style({
+      image: new ol.style.Circle({
+        fill: new ol.style.Fill({
+          color: 'rgba(255,0,255,0.5)'
+        }),
+        radius: 5,
+        stroke: new ol.style.Stroke({
+          color: '#f0f',
+          width: 1
+        })
+      })
+    }),
+    'MultiLineString': new ol.style.Style({
+      stroke: new ol.style.Stroke({
+        color: '#0f0',
+        width: 3
+      })
+    }),
+    'MultiPolygon': new ol.style.Style({
+      fill: new ol.style.Fill({
+        color: 'rgba(0,0,255,0.5)'
+      }),
+      stroke: new ol.style.Stroke({
+        color: '#00f',
+        width: 1
+      })
+    })
+  };
+
+var styleFunction = function(feature, resolution) {
+   var featureStyleFunction = feature.getStyleFunction();
+   if (featureStyleFunction) {
+     return featureStyleFunction.call(feature, resolution);
+   } else {
+     return defaultStyle[feature.getGeometry().getType()];
+   }
+ };
+
 
 function init_map(){
 
@@ -17,6 +83,9 @@ function init_map(){
               })
             })
           ];
+
+
+
 
     proj4.defs("EPSG:27700","+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs");
     var extent = [-3276800, -3276800, 3276800, 3276800];
